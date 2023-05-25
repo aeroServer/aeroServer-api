cc0adda2054ab0172a3398fac8c974cd9ed4f27f
Yann Challet <yann@cymdev.com>
2023-05-25 10:03:12 +0200

    ResourcePicture API

---
+++ app/Http/Resources/PictureResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PictureResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        var_dump($request);
+        exit();
+        return parent::toArray($request);
+    }
+}
